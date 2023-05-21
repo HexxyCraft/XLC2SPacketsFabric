7986ad016586f286fcc745c3141d230085750bd7
object-Object <object@objectobject.ca>
2023-05-21 00:18:49 -0400

    Fix mixin, rename mod to XLC2SPackets

--- src/main/java/tfar/xlc2spackets/mixin/CustomPayloadC2SPacketMixin.java
+++ src/main/java/tfar/xlc2spackets/mixin/CustomPayloadC2SPacketMixin.java
@@ -1,4 +1,4 @@
-package tfar.xlpackets.mixin;
+package tfar.xlc2spackets.mixin;
 
 import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,13 +10,13 @@ public class CustomPayloadC2SPacketMixin {
     // https://github.com/TheRandomLabs/RandomPatches/blob/a2a9f19caab38f3f4078bf5acadcca7e300d4788/src/main/java/com/therandomlabs/randompatches/mixin/client/packetsizelimits/CustomPayloadC2SPacketMixin.java
     int newLimit = 0x1000000;
 
-    @ModifyConstant(method = "<init>(Lnet/minecraft/util/Identifier;Lnet/minecraft/network/PacketByteBuf;)V",constant = @Constant(intValue = 32767))
-    private int xlPackets(int old) {
+    @ModifyConstant(method = "<init>(Lnet/minecraft/network/PacketByteBuf;)V",constant = @Constant(intValue = 32767))
+    private int xlC2SPackets(int old) {
         return newLimit;
     }
 
-    @ModifyConstant(method = "<init>(Lnet/minecraft/util/Identifier;Lnet/minecraft/network/PacketByteBuf;)V",constant = @Constant(stringValue = "Payload may not be larger than 32767 bytes"))
-    private String xlPackets(String old) {
+    @ModifyConstant(method = "<init>(Lnet/minecraft/network/PacketByteBuf;)V",constant = @Constant(stringValue = "Payload may not be larger than 32767 bytes"))
+    private String xlC2SPackets(String old) {
         return "Payload may not be larger than " + newLimit + " bytes";
     }
 }

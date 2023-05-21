12dfc363ec27e2bfb79ccc8bf421bc61cd04c680
object-Object <object@objectobject.ca>
2023-05-20 22:48:47 -0400

    Increase C2S packet limit

---
+++ src/main/java/tfar/xlpackets/mixin/CustomPayloadC2SPacketMixin.java
@@ -0,0 +1,22 @@
+package tfar.xlpackets.mixin;
+
+import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(CustomPayloadC2SPacket.class)
+public class CustomPayloadC2SPacketMixin {
+    // https://github.com/TheRandomLabs/RandomPatches/blob/a2a9f19caab38f3f4078bf5acadcca7e300d4788/src/main/java/com/therandomlabs/randompatches/mixin/client/packetsizelimits/CustomPayloadC2SPacketMixin.java
+    int newLimit = 0x1000000;
+
+    @ModifyConstant(method = "<init>(Lnet/minecraft/util/Identifier;Lnet/minecraft/network/PacketByteBuf;)V",constant = @Constant(intValue = 32767))
+    private int xlPackets(int old) {
+        return newLimit;
+    }
+
+    @ModifyConstant(method = "<init>(Lnet/minecraft/util/Identifier;Lnet/minecraft/network/PacketByteBuf;)V",constant = @Constant(stringValue = "Payload may not be larger than 32767 bytes"))
+    private String xlPackets(String old) {
+        return "Payload may not be larger than " + newLimit + " bytes";
+    }
+}
